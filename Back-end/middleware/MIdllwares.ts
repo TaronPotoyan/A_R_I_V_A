00dc604b18d7e1a32de13a53253e4c8ce5d31aad
Taron Potoyan <potoyantaron@gmail.com>
2025-10-04 22:41:58 +0400

    [UPDATE]

--- Back-end/middleware/MIdllwares.ts
+++ Back-end/middleware/MIdllwares.ts
@@ -3,16 +3,20 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 class Middlwares {
-    private static KEY: string | undefined = process.env.SECRET_KEY;
+    private static KEY: string | undefined = process.env.SECRET_KEY?.trim();
     constructor() {}
     async IsValidKey(req: Request, res: Response, next: NextFunction) {
         try {
-            const { KEY } = req.body;
+            let KEY : string  = req.body.KEY;
+            KEY = KEY.trim();
 
+            console.log(Middlwares.KEY);
             if (Middlwares.KEY !== KEY) {
+                console.log(KEY)
                 return res.status(401).json({ message: 'Invalid Request' });
             }
             next();
+
         } catch (e) {
             console.log(`Error ${e}`);
             res.status(500).json({ message: 'Server error' });
